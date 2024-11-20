cf8ae54da6c5286a5ccb03967d1b092f67530d07
coppercoding555 <seksan1467@gmail.com>
2024-11-20 12:15:21 +0700

    first commit

---
+++ Lab1Task3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Lab1Task3
+{
+  public static void main(String[] args)
+  {
+   Scanner kb = new Scanner(System.in);
+   int [] test = new int[3];
+    for(int i = 0; i<10;i++)
+    {
+     test[i] = kb.nextInt();
+    }
+  }
+}
