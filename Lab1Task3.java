0187b5971b60114a69a3003e03fcee7d36b3a761
Seksan Thichon <seksan1467@gmail.com>
2024-11-20 14:24:57 +0700

    Update Lab1Task3.java

--- Lab1Task3.java
+++ Lab1Task3.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
-public class Lab1Task3
-{
-  public static void main(String[] args)
-  {
-   Scanner kb = new Scanner(System.in);
-   int [] test = new int[3];
-    for(int i = 0; i<10;i++)
-    {
-     test[i] = kb.nextInt();
+public class Lab1Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //input num
+        System.out.print("Num: ");
+        int num = scanner.nextInt();
+ 
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");  
+            }
+            System.out.println("*");
+        }
+ 
+        scanner.close();
     }
-  }
 }
